46bd9a39b850931bea8ad22797931cdae71ddd2a
dlford <dan-github2@dlford.co>
2022-09-30 09:00:45 -0700

    hyper: update font size

--- .hyper.js
+++ .hyper.js
@@ -8,9 +8,9 @@ module.exports = {
         // or `'canary'` for less polished but more frequent updates
         updateChannel: 'stable',
         // default font size in pixels for all tabs
-        fontSize: 12,
+        fontSize: 14,
         // font family with optional fallbacks
-        fontFamily: 'JetBrainsMono Nerd Font Mono, MesloLGSDZ Nerd Font Mono, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+        fontFamily: 'JetBrainsMono NFM, MesloLGSDZ Nerd Font Mono, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
         // default font weight: 'normal' or 'bold'
         fontWeight: 'normal',
         // font weight for bold characters: 'normal' or 'bold'
