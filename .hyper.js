d488ce4747009afd93ef35d9e059123a4930a944
dlford <dan-github2@dlford.co>
2023-09-29 13:03:48 -0700

    hyper: change theme

--- .hyper.js
+++ .hyper.js
@@ -146,8 +146,8 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: [// 'hyper-statusline',
-    'hyper-github-dark', 'hypercwd', 'hyperterm-paste', 'hyper-alt-click', 'hyper-tabs-enhanced', 'hyper-hide-title', 'hyper-hover-header', "hyper-electron-highlighter"],
+    plugins: [// 'hyper-statusline', 'hyperterm-material-spacegray'
+    'hypercwd', 'hyperterm-paste', 'hyper-alt-click', 'hyper-tabs-enhanced', 'hyper-hide-title', 'hyper-hover-header', 'hyperterm-material-spacegray'],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
